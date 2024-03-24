d73749bf84a2fd226984db7961bbfbc3d3611ac5
Hyungwon Yang <hyung8758@gmail.com>
2024-03-24 23:55:27 +0900

    two instances (imagehandler and texthandler) initialization starts at the beginning

--- src/handlers/ServerHandler.py
+++ src/handlers/ServerHandler.py
@@ -11,6 +11,8 @@ import uvicorn
 import daemon
 import daemon.pidfile
 import signal
+from src.handlers.ImageHandler import ImageHandler
+from src.handlers.TextHandler import TextHandler
 from fastapi import FastAPI, Request
 
 cur_dir = os.getcwd()
@@ -23,9 +25,21 @@ app = FastAPI()
 @app.post("/text2image")
 async def post(request: Request):
     logging.info("start post request.")
+    # Retrieve the handlers using dependency injection
+    image_handler = request.app.state.image_handler
+    text_handler = request.app.state.text_handler
+    
     json_data = await request.json()
     logging.info(json_data)
-    response = {"message": "Hello. This is text2image server"}
+    input_text = json_data['input_text']
+    logging.info("INPUT TEXT: {}".format(input_text))
+    if text_handler:
+        input_text = text_handler(input_text)
+        logging.info("text_handler output: {}".format(input_text))
+    if image_handler:
+        output_image = image_handler(input_text)
+    logging.info("image_handler output: {}".format(output_image))
+    response = {"message": "successul", "output": output_image}
     logging.info("done post request.")
     return response
 
@@ -40,6 +54,18 @@ def start_server() -> None:
     if os.path.exists(pid_file_path):
         logging.error("server is already running {}:{}".format(server_host, server_port))
     else:
+        # task handlers.
+        imageHandler = ImageHandler()
+        textHandler = TextHandler()
+        imageHandler.initialize(model="karlo")
+        textHandler.initialize(model="ke-t5")
+        # Assign the handlers to the app's state
+        app.state.image_handler = imageHandler
+        app.state.text_handler = textHandler
+        # kwargs = dict(
+        #     image_handler = imageHandler,
+        #     text_handler = textHandler
+        # )
         with daemon.DaemonContext(
                 pidfile=daemon.pidfile.PIDLockFile(os.path.join(pid_path, "server.pid")),
                 stdout=sys.stdout,
