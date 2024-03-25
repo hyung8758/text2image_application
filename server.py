89fd8dce981f985c3b2ab25d418b59e731025e86
Hyungwon Yang <hyung8758@gmail.com>
2024-03-25 23:47:06 +0900

    add console mode

--- server.py
+++ server.py
@@ -7,28 +7,31 @@ Hyungwon Yang
 
 import os, sys
 import time
-from src.handlers.ServerHandler import start_server, stop_server
+from src.handlers.ServerHandler import start_server, stop_server, console
 from src.utils.LogUtils import LogUtils
 
 # main daemon server.
 def main():
     if len(sys.argv) != 2:
-        print("Usage: {} [start|stop|restart]".format(sys.argv[0]))
+        print("Usage: {} [start|stop|restart|console]".format(sys.argv[0]))
         sys.exit(2)
     else:
-        log_path = os.path.join(os.getcwd(), "log")
-        LogUtils.init_log(log_path)
-        if sys.argv[1] == 'start':
-            start_server()
-        elif sys.argv[1] == 'stop':
-            stop_server()
-        elif sys.argv[1] == 'restart':
-            stop_server()
-            time.sleep(1)
-            start_server()
+        if sys.argv[1] == 'console':
+            console()
         else:
-            print("Unknown command")
-            sys.exit(2)
+            log_path = os.path.join(os.getcwd(), "log")
+            LogUtils.init_log(log_path)
+            if sys.argv[1] == 'start':
+                start_server()
+            elif sys.argv[1] == 'stop':
+                stop_server()
+            elif sys.argv[1] == 'restart':
+                stop_server()
+                time.sleep(2)
+                start_server()
+            else:
+                print("Unknown command")
+                sys.exit(2)
 
 if __name__ == "__main__":
     main()
