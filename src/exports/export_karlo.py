c09549e8b750e71793d18e31d98390138a2f562c
Hyungwon Yang <hyung8758@gmail.com>
2024-04-22 16:42:53 +0900

    sr model is saved as a external data type. split onnx files are now combiend.

--- src/exports/export_karlo.py
+++ src/exports/export_karlo.py
@@ -1,5 +1,6 @@
 """
 export_karlo : download karlo model and export it to onnx.
+This code will be reorganized later.
 
 Hyungwon Yang
 """
@@ -31,14 +32,17 @@ CKPT_PATH = {
     "clip_stat" : "ViT-L-14_stats.th"
 }
 
-def export_karlo_from_source():
+def export_karlo_from_source(cuda_device:int=-1):
     import os, sys
     CURRENT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../..")
     sys.path.append(CURRENT_PATH)
     os.chdir(CURRENT_PATH)
     print("current: {}".format(CURRENT_PATH))
     import torch
+    import shutil
     import numpy as np
+    import onnx
+    from onnx.external_data_helper import convert_model_to_external_data
     import torchvision.transforms.functional as TVF
     from PIL import Image
     from omegaconf import OmegaConf
@@ -48,102 +52,37 @@ def export_karlo_from_source():
     from src.lib.karlo.karlo.models.sr_64_256 import ImprovedSupRes64to256ProgressiveModel
     from src.lib.karlo.karlo.models.clip import CustomizedCLIP, CustomizedTokenizer
 
-    ## inputs
-    prompt = "a high-resolution photograph of a big red frog on a green leaf."
-    prior_num_inference_steps=25
-    decoder_num_inference_steps=25
-    super_res_num_inference_steps=7
 
+    device="cuda" if cuda_device == -1 else f"cuda:{cuda_device}"
+    # save path
     model_path = "karlo_models"
-    batch_size=1
-    sampling_type="fast"
+    prior_save_path = "save_prior_model"
+    decoder_save_path = "save_decode_model"
+    sr_save_path = "save_sr_model"
+    model_full_path = os.path.join(CURRENT_PATH, model_path)
+    prior_save_full_path = os.path.join(CURRENT_PATH, prior_save_path)
+    decoder_save_full_path = os.path.join(CURRENT_PATH, decoder_save_path)
+    sr_save_full_path = os.path.join(CURRENT_PATH, sr_save_path)
+    if not os.path.exists(model_full_path):
+        os.makedirs(model_full_path)
+    if not os.path.exists(prior_save_full_path):
+        os.makedirs(prior_save_full_path)
+    if not os.path.exists(decoder_save_full_path):
+        os.makedirs(decoder_save_full_path)
+    if not os.path.exists(sr_save_full_path):
+        os.makedirs(sr_save_full_path)
 
-    # models
     # tokenizer
     tokenizer_model=CustomizedTokenizer()
     # clip
     clip_model = CustomizedCLIP.load_from_checkpoint(
                 os.path.join(CURRENT_PATH, model_path, CKPT_PATH['clip']))
     clip_model = torch.jit.script(clip_model)
-    clip_model.cuda(device=1)
+    clip_model.to(device=device)
     clip_model.eval()
     clip_mean, clip_std = torch.load(
         os.path.join(CURRENT_PATH, model_path, CKPT_PATH['clip_stat']), map_location="cpu")
 
-    # # prior
-    # prior_config = OmegaConf.load(os.path.join(model_path, "prior_1B_vit_l.yaml"))
-    # prior_model = PriorDiffusionModel.load_from_checkpoint(
-    #     prior_config,
-    #     tokenizer_model,
-    #     clip_mean,
-    #     clip_std,
-    #     os.path.join(CURRENT_PATH, model_path, CKPT_PATH['prior']),
-    #     strict=True,
-    # )
-    # prior_model.cuda(device=1)
-    # prior_model.eval()
-    # # decoders
-    # decoder_config =OmegaConf.load(os.path.join(model_path, "decoder_900M_vit_l.yaml"))
-    # decoder_model = Text2ImProgressiveModel.load_from_checkpoint(
-    #     decoder_config,
-    #     tokenizer_model,
-    #     os.path.join(CURRENT_PATH, model_path, CKPT_PATH['decoder']),
-    #     strict=True,
-    # )
-    # decoder_model.cuda(device=1)
-    # decoder_model.eval()
-    # # super resolution
-    # sr_config = OmegaConf.load(os.path.join(model_path, "improved_sr_64_256_1.4B.yaml"))
-    # sr_model = ImprovedSupRes64to256ProgressiveModel.load_from_checkpoint(
-    #     sr_config, 
-    #     os.path.join(CURRENT_PATH, model_path, CKPT_PATH['sr_256']), strict=True
-    # )
-    # sr_model.cuda(device=1)
-    # sr_model.eval()
-
-
-    # # """Setup prompts & cfg scales"""
-    # prompts_batch = [prompt for _ in range(batch_size)]
-
-    # sampling_type = SAMPLING_CONF[sampling_type]
-    # prior_sm = sampling_type["prior_sm"]
-    # prior_n_samples = sampling_type["prior_n_samples"]
-    # prior_cf_scale = sampling_type["prior_cf_scale"]
-    # decoder_sm = sampling_type["decoder_sm"]
-    # decoder_cf_scale = sampling_type["decoder_cf_scale"]
-    # sr_sm = sampling_type["sr_sm"]
-            
-    # prior_cf_scales_batch = [prior_cf_scale] * len(prompts_batch)
-    # prior_cf_scales_batch = torch.tensor(prior_cf_scales_batch, device="cuda:1")
-
-    # decoder_cf_scales_batch = [decoder_cf_scale] * len(prompts_batch)
-    # decoder_cf_scales_batch = torch.tensor(decoder_cf_scales_batch, device="cuda:1")
-
-    # # """ Get CLIP text feature """
-    # max_txt_length = prior_model.model.text_ctx
-
-    # tok, mask = tokenizer_model.padded_tokens_and_mask(prompts_batch, max_txt_length)
-    # cf_token, cf_mask = tokenizer_model.padded_tokens_and_mask([""], max_txt_length)
-    # if not (cf_token.shape == tok.shape):
-    #     cf_token = cf_token.expand(tok.shape[0], -1)
-    #     cf_mask = cf_mask.expand(tok.shape[0], -1)
-
-    # tok = torch.cat([tok, cf_token], dim=0)
-    # mask = torch.cat([mask, cf_mask], dim=0)
-
-    # tok, mask = tok.to(device="cuda:1"), mask.to(dtype=torch.bool, device="cuda:1")
-    # txt_feat, txt_feat_seq = clip_model.encode_text(tok)
-
-    # # get prior
-    # timestep_respacing="10"
-    # img_feat = prior_model(
-    #                 txt_feat,
-    #                 txt_feat_seq,
-    #                 mask,
-    #                 prior_cf_scales_batch,
-    #                 timestep_respacing=timestep_respacing,
-    #             )
-    
     # Export prior_model
     print("Export prior model to onnx format.")
     with torch.autocast("cuda", dtype=torch.float16):
@@ -156,18 +95,18 @@ def export_karlo_from_source():
             os.path.join(CURRENT_PATH, model_path, CKPT_PATH['prior']),
             strict=True,
         )
-        prior_model.cuda(device=1)
+        prior_model.to(device=device)
         prior_model.eval()
         
-        txt_feat=torch.randn(2,768).to(device="cuda:1")
-        txt_feat_seq=torch.randn(2,77,768).to(device="cuda:1")
-        mask=torch.ones(2,77, dtype=torch.bool).to(device="cuda:1")
-        prior_cf_scales_batch=torch.tensor([0.4]).to(device="cuda:1")
+        txt_feat=torch.randn(2,768).to(device=device)
+        txt_feat_seq=torch.randn(2,77,768).to(device=device)
+        mask=torch.ones(2,77, dtype=torch.bool).to(device=device)
+        prior_cf_scales_batch=torch.tensor([0.4]).to(device=device)
         timestep_respacing="2"
         torch.onnx.export(
             prior_model,
             (txt_feat, txt_feat_seq, mask, prior_cf_scales_batch, timestep_respacing),
-            "save_model_point/prior_model.onnx",
+            f"{prior_save_full_path}/prior_model.onnx",
             input_names=["txt_feat", "txt_feat_seq", "mask", "prior_cf_scales_batch", "timestep_respacing"],
             output_names=["img_feat"],
             dynamic_axes={
@@ -179,19 +118,9 @@ def export_karlo_from_source():
             opset_version=14,
             do_constant_folding=True,
         )
-    
-    # timestep_respacing=decoder_sm
-    # images_64_outputs = decoder_model(
-    #                 txt_feat,
-    #                 txt_feat_seq,
-    #                 tok,
-    #                 mask,
-    #                 img_feat,
-    #                 cf_guidance_scales=decoder_cf_scales_batch,
-    #                 timestep_respacing=timestep_respacing,
-    #             )
 
     # Export decoder_model
+    # NOTE: need to fix source to run the following lines.
     print("Export decoder model to onnx format.")
     with torch.autocast("cuda", dtype=torch.float16):
         # load decoder model.
@@ -202,20 +131,20 @@ def export_karlo_from_source():
             os.path.join(CURRENT_PATH, model_path, CKPT_PATH['decoder']),
             strict=True,
         )
-        decoder_model.cuda(device=1)
+        decoder_model.to(device=device)
         decoder_model.eval()
         
-        txt_feat=torch.randn(2,768).to(device="cuda:1")
-        txt_feat_seq=torch.randn(2,77,768).to(device="cuda:1")
-        tok=torch.ones(2,77).to(dtype=torch.int32, device="cuda:1")
-        mask=torch.ones(2,77).to(dtype=torch.bool, device="cuda:1")
-        img_feat=torch.randn(1,768).to(device="cuda:1")
-        decoder_cf_scales_batch=torch.tensor([0.4]).to(device="cuda:1")
+        txt_feat=torch.randn(2,768).to(device=device)
+        txt_feat_seq=torch.randn(2,77,768).to(device=device)
+        tok=torch.ones(2,77).to(dtype=torch.int32, device=device)
+        mask=torch.ones(2,77).to(dtype=torch.bool, device=device)
+        img_feat=torch.randn(1,768).to(device=device)
+        decoder_cf_scales_batch=torch.tensor([0.4]).to(device=device)
         timestep_respacing="2"
         torch.onnx.export(
             decoder_model,
             (txt_feat, txt_feat_seq, tok, mask, img_feat, decoder_cf_scales_batch, timestep_respacing),
-            "save_model_point/decoder_model.onnx",
+            f"{decoder_save_full_path}/decoder_model.onnx",
             input_names=["txt_feat", "txt_feat_seq", "tok", "mask", "img_feat", "decoder_cf_scales_batch", "timestep_respacing"],
             output_names=["images_64"],
             dynamic_axes={
@@ -232,26 +161,9 @@ def export_karlo_from_source():
             do_constant_folding=True,
         )
 
-    # images_64 = None
-    # for k, out in enumerate(images_64_outputs):
-    #     print(k)
-    #     images_64 = out
-
-    # images_64 = torch.clamp(images_64, -1, 1)
-
-    # """ Upsample 64x64 to 256x256 """
-    # images_256 = TVF.resize(
-    #     images_64,
-    #     [256, 256],
-    #     interpolation=InterpolationMode.BICUBIC,
-    #     antialias=True,
-    # )
-    # images_256_outputs = sr_model(
-    #     images_256, timestep_respacing=sr_sm
-    # )
-
     # Export sr_model
-    # NOTE: too large model! so it generates multiple onnx files instead of a single file.
+    # NOTE: need to fix source to run the following lines.
+    #       This model is too large to export! so it generates multiple onnx files instead of a single file.
     print("Export super resolution model to onnx format.")
     with torch.autocast("cuda", dtype=torch.float16):
         # load super resolution model.
@@ -260,15 +172,17 @@ def export_karlo_from_source():
             sr_config, 
             os.path.join(CURRENT_PATH, model_path, CKPT_PATH['sr_256']), strict=True
         )
-        sr_model.cuda(device=1)
+        sr_model.to(device=device)
         sr_model.eval()
+        sr_save_model_path = f"{sr_save_full_path}/sr_model.onnx"
+        sr_save_external_path = f"{sr_save_full_path}/sr_external_model.onnx"
         
-        images_256=torch.randn(1, 3, 256, 256).to(device="cuda:1")
+        images_256=torch.randn(1, 3, 256, 256).to(device=device)
         sr_sm="7"
         torch.onnx.export(
             sr_model,
             (images_256, sr_sm),
-            "save_model_point/sr_model.onnx",
+            sr_save_model_path,
             input_names=["images_256", "sr_sm"],
             output_names=["generated_image"],
             dynamic_axes={
@@ -278,18 +192,22 @@ def export_karlo_from_source():
             opset_version=14,
             do_constant_folding=True,
         )
+        
+        # Once it saved into multiple files, read the main onnx file again and save a large part of networks to an external data type.
+        # Onnx files will be saved into two split files.
+        print("Combine the large split onnx files into an external data type")
+        sr_onnx_model = onnx.load(sr_save_model_path)
+        # remove previous onnx file.
+        if os.path.exists(sr_save_full_path):
+            shutil.rmtree(sr_save_full_path)
+            os.makedirs(sr_save_full_path)
+        convert_model_to_external_data(sr_onnx_model, all_tensors_to_one_file=True, location=sr_save_external_path, size_threshold=1024, convert_attribute=False)
+        onnx.save_model(sr_onnx_model, sr_save_model_path)
 
-    # for k, out in enumerate(images_256_outputs):
-    #     print(k)
-    #     images_256 = out
 
-    # generated_image = torch.clamp(images_256 * 0.5 + 0.5, 0.0, 1.0)
-
-    # images = (torch.permute(generated_image * 255.0, [0, 2, 3, 1]).type(torch.uint8).cpu().numpy())
-    # image = Image.fromarray(images[0])
-
-def export_karlo_from_library():
-    
+def export_karlo_from_library(cuda_device:int=-1):
+    print("Not yet implemented. Use source type.")
+    exit(1)
     import torch
     from diffusers import UnCLIPPipeline
 
@@ -322,83 +240,21 @@ def export_karlo_from_library():
         dynamic_axes={"prompt": {0: "batch", 1: "sequence"}},  # Specify dynamic axes if needed
         do_constant_folding=True,  # Perform constant folding
     )
-
-    # # tokenizer
-    # tokenized_text = karlo_tokenizer(
-    #                 [prompt],
-    #                 padding="max_length",
-    #                 max_length=karlo_tokenizer.model_max_length,
-    #                 truncation=True,
-    #                 return_tensors="pt",
-    #                 ).input_ids
-    # tokenized_text_uncond = karlo_tokenizer(
-    #                 [""],
-    #                 padding="max_length",
-    #                 max_length=karlo_tokenizer.model_max_length,
-    #                 truncation=True,
-    #                 return_tensors="pt",
-    #                 ).input_ids
-
-    # # text input preprocessing
-    # # Querying the text_encoding model
-    # input_ids_1 = pb_utils.Tensor(
-    #     "input_ids",
-    #     np.concatenate(
-    #         [
-    #             tokenized_text_uncond.numpy().astype(np.int32),
-    #             tokenized_text.numpy().astype(np.int32),
-    #         ]
-    #     ),
-    # )
-    # encoding_request = pb_utils.InferenceRequest(
-    #     model_name="text_encoder",
-    #     requested_output_names=["last_hidden_state"],
-    #     inputs=[input_ids_1],
-    # )
-
-    # response = encoding_request.exec()
-    # # text embedding
-    # if response.has_error():
-    #     raise pb_utils.TritonModelException(response.error().message())
-    # else:
-    #     text_embeddings = pb_utils.get_output_tensor_by_name(
-    #         response, "last_hidden_state"
-    #     )
-    # text_embeddings = from_dlpack(text_embeddings.to_dlpack()).clone()
-    # text_embeddings = text_embeddings.to("cuda")
-
-
-
-
-    # image = pipe(prompt).images[0]
-
-    # torch.onnx.export(
-    #     karlo_image_generator,
-    #     (prompt),
-    #     "karlo_image_generator.onnx",
-    #     input_names=["input_ids"],
-    #     output_names=["last_hidden_state", "pooler_output"],
-    #     dynamic_axes={
-    #         "input_ids": {0: "batch", 1: "sequence"},
-    #     },
-    #     opset_version=14,
-    #     do_constant_folding=True,
-    # )
-    # decode_result = pipe(prompt)
-
-
+    
+    
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
         
     group = parser.add_argument_group("Basic configuration")
     group.add_argument("--type","-t", type=str, default="source", help="Onnx export type: source or library")
+    group.add_argument("--cuda_device","-d", type=int, default=-1, help="Use specific cuda device. -1 means all. e.g., set 0 to use 1st gpu device.")
     args = parser.parse_args()
     
     print("export type: {}".format(args.type))
     if args.type == "source":
-        export_karlo_from_source()
+        export_karlo_from_source(cuda_device=args.cuda_device)
     elif args.type == "library":
-        export_karlo_from_library()
+        export_karlo_from_library(cuda_device=args.cuda_device)
     else:
         raise RuntimeError("Unknown arguments: {}".format(args.type))
