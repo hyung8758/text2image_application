848f2bcb55f70a55964eaa16e68c9426782d7793
Hyungwon Yang <hyung8758@gmail.com>
2024-06-10 16:55:14 +0900

    export karlo model to onnx format is updated

--- src/exports/export_karlo.py
+++ src/exports/export_karlo.py
@@ -5,6 +5,19 @@ This code will be reorganized later.
 Hyungwon Yang
 """
 
+import os, sys
+import requests
+import torch
+import shutil
+import onnx
+from onnx.external_data_helper import convert_model_to_external_data
+from omegaconf import OmegaConf
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from src.lib.karlo.karlo.models.prior_model import PriorDiffusionModel
+from src.lib.karlo.karlo.models.decoder_model import Text2ImProgressiveModel
+from src.lib.karlo.karlo.models.sr_64_256 import ImprovedSupRes64to256ProgressiveModel
+from src.lib.karlo.karlo.models.clip import CustomizedCLIP, CustomizedTokenizer
+
 SAMPLING_CONF = {
     "default": {
         "prior_sm": "25",
@@ -24,6 +37,14 @@ SAMPLING_CONF = {
     },
 }
 
+MODEL_PATH = {
+    "prior-ckpt-step=01000000-of-01000000.ckpt": "https://arena.kakaocdn.net/brainrepo/models/karlo-public/v1.0.0.alpha/85626483eaca9f581e2a78d31ff905ca/prior-ckpt-step%3D01000000-of-01000000.ckpt",
+    "decoder-ckpt-step=01000000-of-01000000.ckpt": "https://arena.kakaocdn.net/brainrepo/models/karlo-public/v1.0.0.alpha/efdf6206d8ed593961593dc029a8affa/decoder-ckpt-step%3D01000000-of-01000000.ckpt",
+    "improved-sr-ckpt-step=1.2M.ckpt" : "https://arena.kakaocdn.net/brainrepo/models/karlo-public/v1.0.0.alpha/4226b831ae0279020d134281f3c31590/improved-sr-ckpt-step%3D1.2M.ckpt",
+    "ViT-L-14.pt": "https://arena.kakaocdn.net/brainrepo/models/karlo-public/v1.0.0.alpha/096db1af569b284eb76b3881534822d9/ViT-L-14.pt",
+    "ViT-L-14_stats.th": "https://arena.kakaocdn.net/brainrepo/models/karlo-public/v1.0.0.alpha/0b62380a75e56f073e2844ab5199153d/ViT-L-14_stats.th"
+}
+
 CKPT_PATH = {
     "prior": "prior-ckpt-step=01000000-of-01000000.ckpt",
     "decoder": "decoder-ckpt-step=01000000-of-01000000.ckpt",
@@ -32,39 +53,28 @@ CKPT_PATH = {
     "clip_stat" : "ViT-L-14_stats.th"
 }
 
-def export_karlo_from_source(cuda_device:int=-1):
-    import os, sys
-    CURRENT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../..")
-    sys.path.append(CURRENT_PATH)
-    os.chdir(CURRENT_PATH)
-    print("current: {}".format(CURRENT_PATH))
-    import torch
-    import shutil
-    import numpy as np
-    import onnx
-    from onnx.external_data_helper import convert_model_to_external_data
-    import torchvision.transforms.functional as TVF
-    from PIL import Image
-    from omegaconf import OmegaConf
-    from torchvision.transforms import InterpolationMode
-    from src.lib.karlo.karlo.models.prior_model import PriorDiffusionModel
-    from src.lib.karlo.karlo.models.decoder_model import Text2ImProgressiveModel
-    from src.lib.karlo.karlo.models.sr_64_256 import ImprovedSupRes64to256ProgressiveModel
-    from src.lib.karlo.karlo.models.clip import CustomizedCLIP, CustomizedTokenizer
+def download_karlo_models(save_path: str = "./models/karlo_models") -> None:
+    for each_file_name in MODEL_PATH.keys():
+        save_file_nmae = os.path.join(save_path, each_file_name)
+        if os.path.exists(save_file_nmae):
+            continue
+        print("Downloading {}".format(each_file_name))
+        with requests.get(MODEL_PATH[each_file_name], stream=True) as r:
+            r.raise_for_status()  # Check for HTTP errors
+            with open(save_file_nmae, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192): 
+                    f.write(chunk)  # Write the downloaded content to the file
 
+def export_karlo_models(save_path: str = "./models/karlo_models", cuda_device:int=-1):
 
     device="cuda" if cuda_device == -1 else f"cuda:{cuda_device}"
     # save path
-    model_path = "karlo_models"
-    prior_save_path = "save_prior_model"
-    decoder_save_path = "save_decode_model"
-    sr_save_path = "save_sr_model"
-    model_full_path = os.path.join(CURRENT_PATH, model_path)
-    prior_save_full_path = os.path.join(CURRENT_PATH, prior_save_path)
-    decoder_save_full_path = os.path.join(CURRENT_PATH, decoder_save_path)
-    sr_save_full_path = os.path.join(CURRENT_PATH, sr_save_path)
-    if not os.path.exists(model_full_path):
-        os.makedirs(model_full_path)
+    prior_save_path = "prior_onnx"
+    decoder_save_path = "decode_onnx"
+    sr_save_path = "sr_onnx"
+    prior_save_full_path = os.path.join(save_path, prior_save_path)
+    decoder_save_full_path = os.path.join(save_path, decoder_save_path)
+    sr_save_full_path = os.path.join(save_path, sr_save_path)
     if not os.path.exists(prior_save_full_path):
         os.makedirs(prior_save_full_path)
     if not os.path.exists(decoder_save_full_path):
@@ -76,23 +86,23 @@ def export_karlo_from_source(cuda_device:int=-1):
     tokenizer_model=CustomizedTokenizer()
     # clip
     clip_model = CustomizedCLIP.load_from_checkpoint(
-                os.path.join(CURRENT_PATH, model_path, CKPT_PATH['clip']))
+                os.path.join(save_path, CKPT_PATH['clip']))
     clip_model = torch.jit.script(clip_model)
     clip_model.to(device=device)
     clip_model.eval()
     clip_mean, clip_std = torch.load(
-        os.path.join(CURRENT_PATH, model_path, CKPT_PATH['clip_stat']), map_location="cpu")
+        os.path.join(save_path, CKPT_PATH['clip_stat']), map_location="cpu")
 
     # Export prior_model
     print("Export prior model to onnx format.")
     with torch.autocast("cuda", dtype=torch.float16):
-        prior_config = OmegaConf.load(os.path.join(model_path, "prior_1B_vit_l.yaml"))
+        prior_config = OmegaConf.load(os.path.join(save_path, "prior_1B_vit_l.yaml"))
         prior_model = PriorDiffusionModel.load_from_checkpoint(
             prior_config,
             tokenizer_model,
             clip_mean,
             clip_std,
-            os.path.join(CURRENT_PATH, model_path, CKPT_PATH['prior']),
+            os.path.join(save_path, CKPT_PATH['prior']),
             strict=True,
         )
         prior_model.to(device=device)
@@ -124,11 +134,11 @@ def export_karlo_from_source(cuda_device:int=-1):
     print("Export decoder model to onnx format.")
     with torch.autocast("cuda", dtype=torch.float16):
         # load decoder model.
-        decoder_config =OmegaConf.load(os.path.join(model_path, "decoder_900M_vit_l.yaml"))
+        decoder_config =OmegaConf.load(os.path.join(save_path, "decoder_900M_vit_l.yaml"))
         decoder_model = Text2ImProgressiveModel.load_from_checkpoint(
             decoder_config,
             tokenizer_model,
-            os.path.join(CURRENT_PATH, model_path, CKPT_PATH['decoder']),
+            os.path.join(save_path, CKPT_PATH['decoder']),
             strict=True,
         )
         decoder_model.to(device=device)
@@ -167,15 +177,15 @@ def export_karlo_from_source(cuda_device:int=-1):
     print("Export super resolution model to onnx format.")
     with torch.autocast("cuda", dtype=torch.float16):
         # load super resolution model.
-        sr_config = OmegaConf.load(os.path.join(model_path, "improved_sr_64_256_1.4B.yaml"))
+        sr_config = OmegaConf.load(os.path.join(save_path, "improved_sr_64_256_1.4B.yaml"))
         sr_model = ImprovedSupRes64to256ProgressiveModel.load_from_checkpoint(
             sr_config, 
-            os.path.join(CURRENT_PATH, model_path, CKPT_PATH['sr_256']), strict=True
+            os.path.join(save_path, CKPT_PATH['sr_256']), strict=True
         )
         sr_model.to(device=device)
         sr_model.eval()
         sr_save_model_path = f"{sr_save_full_path}/sr_model.onnx"
-        sr_save_external_path = f"{sr_save_full_path}/sr_external_model.onnx"
+        sr_save_external_path = "sr_external_model.onnx" # f"{sr_save_full_path}/sr_external_model.onnx"
         
         images_256=torch.randn(1, 3, 256, 256).to(device=device)
         sr_sm="7"
@@ -204,57 +214,15 @@ def export_karlo_from_source(cuda_device:int=-1):
         convert_model_to_external_data(sr_onnx_model, all_tensors_to_one_file=True, location=sr_save_external_path, size_threshold=1024, convert_attribute=False)
         onnx.save_model(sr_onnx_model, sr_save_model_path)
 
-
-def export_karlo_from_library(cuda_device:int=-1):
-    print("Not yet implemented. Use source type.")
-    exit(1)
-    import torch
-    from diffusers import UnCLIPPipeline
-
-    karlo_image_generator = UnCLIPPipeline.from_pretrained("kakaobrain/karlo-v1-alpha", torch_dtype=torch.float32)
-    # pipe.save_pretrained("./karlo_models", from_pt=True)
-    # karlo_image_generator = karlo_image_generator.to('cuda:1')
-    # initialization
-    karlo_tokenizer = karlo_image_generator.tokenizer
-    karlo_text_encoder = karlo_image_generator.text_encoder
-    karlo_text_proj = karlo_image_generator.text_proj
-
-    karlo_prior = karlo_image_generator.prior
-    karlo_decoder = karlo_image_generator.decoder
-
-    karlo_sr_fist = karlo_image_generator.super_res_first
-    karlo_sr_last = karlo_image_generator.super_res_last
-
-    prompt = "a high-resolution photograph of a big red frog on a green leaf."
-    prior_num_inference_steps=25
-    decoder_num_inference_steps=25
-    super_res_num_inference_steps=7
-
-    torch.onnx.export(
-        karlo_image_generator,
-        (prompt, prior_num_inference_steps, decoder_num_inference_steps, super_res_num_inference_steps),
-        "karlo_image_generator.onnx",
-        input_names=["prompt", "prior_num_inference_steps", "decoder_num_inference_steps", "super_res_num_inference_steps"],
-        output_names=["generated_image"],
-        opset_version=12,  # Use a suitable ONNX opset version
-        dynamic_axes={"prompt": {0: "batch", 1: "sequence"}},  # Specify dynamic axes if needed
-        do_constant_folding=True,  # Perform constant folding
-    )
-    
     
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
         
     group = parser.add_argument_group("Basic configuration")
-    group.add_argument("--type","-t", type=str, default="source", help="Onnx export type: source or library")
     group.add_argument("--cuda_device","-d", type=int, default=-1, help="Use specific cuda device. -1 means all. e.g., set 0 to use 1st gpu device.")
     args = parser.parse_args()
     
-    print("export type: {}".format(args.type))
-    if args.type == "source":
-        export_karlo_from_source(cuda_device=args.cuda_device)
-    elif args.type == "library":
-        export_karlo_from_library(cuda_device=args.cuda_device)
-    else:
-        raise RuntimeError("Unknown arguments: {}".format(args.type))
+    # Step 1. download karlo model.
+    download_karlo_models()
+    export_karlo_models(cuda_device=args.cuda_device)
